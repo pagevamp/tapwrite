dfefa3b81d65cec8e77a914b355f5bc7b9c8cadd
arpandhakal <arpandhakal20@gmail.com>
2026-01-13 17:28:02 +0545

    fix(OUT-2864): enabled sourcemaps and removed minified errors from tapwrite

--- vite.config.ts
+++ vite.config.ts
@@ -6,34 +6,36 @@ import { extname, relative, resolve } from 'path'
 import { fileURLToPath } from 'node:url'
 import { glob } from 'glob'
 
-
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'], insertTypesEntry: true }), libInjectCss()],
+  plugins: [
+    react(),
+    dts({ include: ['lib'], insertTypesEntry: true }),
+    libInjectCss(),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      formats: ['es'],
     },
+    sourcemap: true,
+    minify: false,
     rollupOptions: {
       external: ['react', 'react/jsx-runtime', 'tailwindcss'],
       input: Object.fromEntries(
-        glob.sync('lib/**/*.{ts,tsx}').map(file => [
+        glob.sync('lib/**/*.{ts,tsx}').map((file) => [
           // The name of the entry point
           // lib/nested/foo.ts becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
+          relative('lib', file.slice(0, file.length - extname(file).length)),
           // The absolute path to the entry file
           // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
+          fileURLToPath(new URL(file, import.meta.url)),
         ])
       ),
       output: {
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
-      }
+      },
     },
   },
 })
